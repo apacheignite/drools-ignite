b5ec9438cf24185986ca2122ba6e3b1a84a09f7d
glutters <mitenick@gmail.com>
2016-03-09 18:59:12 +0100

    Define the base classes

--- src/main/java/org/drools/persistence/ignite/processinstance/IgniteProcessInstanceManagerFactory.java
+++ src/main/java/org/drools/persistence/ignite/processinstance/IgniteProcessInstanceManagerFactory.java
@@ -16,8 +16,5 @@ package org.drools.persistence.ignite.processinstance;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-public class IgniteProcessInstanceManager {
-
-
-
+public class IgniteProcessInstanceManagerFactory {
 }
